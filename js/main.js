f5f9b023e2f7aa02ad141839c71d03bc1d1b6106
gabc123123 <gabc123123@gmail.com>
2023-02-16 09:41:22 +0200

    update

--- js/main.js
+++ js/main.js
@@ -363,18 +363,18 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
 // addBtn.style.display = 'block';
-printMsg('fApp', `<span id="fApp"><a href="/app.${confExt}">app: Install app</a></span>`); 
+printMsg('fApp', `<a href="/app.${confExt}">app: Install app</a>`); 
 addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
 //addBtn.style.display = 'none';
-printMsg('fApp', `<span id="fApp"><a href="/app.${confExt}">app: Install in progress</a></span>`); 
+printMsg('fApp', `<a href="/app.${confExt}">app: Install in progress</a>`); 
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
 //        console.log('app: User accepted the app prompt');
-addBtn.innerHTML = `<span id="fApp"><a href="/app.${confExt}">app: User accepted the app prompt</a></span>`;
+printMsg('fApp', `<a href="/app.${confExt}">app: User accepted the app prompt</a>`); 
       } else {
 //        console.log('app: User dismissed the app prompt');
 addBtn.innerHTML = '<span id="fApp"><a href="/app.${confExt}"app: User dismissed the app prompt</a></span>';
