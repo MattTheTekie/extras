355c08fdbe7e1fd7d20784398a6a83ac645b93d2
gabc123123 <gabc123123@gmail.com>
2023-02-16 04:35:34 +0200

    update

--- firefox-extensions/custom-new-tab/script.js
+++ firefox-extensions/custom-new-tab/script.js
@@ -24,7 +24,7 @@ newTabRedir(rUrl);
 
 function newTabRedir(rUrl){
 
-//if(String(window.location.href).indexOf('moz-extension://') >= 0&&String(window.location.href).indexOf('/custom-new-tab.html') >= 0){ // prevent infinite redirect
+//if(String(window.location.href).indexOf('moz-extension://') >= 0&&String(window.location.href).indexOf('/custom-new-tab.html') >= 0){ 
 
 
 // stop infinite redirect current page
