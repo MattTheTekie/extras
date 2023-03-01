47e735d9c042131122895b9c5d46494556d6cc83
gabc123123 <gabc123123@gmail.com>
2023-03-01 08:19:54 +0200

    update

---
+++ data/blogJsonVar.js
@@ -0,0 +1,45 @@
+var blogJsonVar = 
+[
+    {
+        "id": 258,
+        "text": "HUBBLE on Twitter: \"Our #HubbleTopImage shows a pair of space oddities \ud83c\udf00\ud83c\udf00\ud83c\udf00 also known as Wild's Triplet, around 200 million light-years from Earth in the constellation Virgo. Read more about these galaxies here: https:\/\/t.co\/AzHqkqM4PZ https:\/\/t.co\/x5sP6dWofe\" \/ Twitter",
+        "url": "https:\/\/twitter.com\/HUBBLE_space\/status\/1626235250060988418",
+        "tag": "#space",
+        "time": 1676955486
+    },
+    {
+        "id": 257,
+        "text": "SpaceX on Twitter: \"Super Heavy Booster 7 completed a full duration static fire test of 31 Raptor engines, producing 7.9 million lbf of thrust (~3,600 metric tons) \u2013 less than half of the booster\u2019s capability https:\/\/t.co\/cNLmp3Pn7G\" \/ Twitter",
+        "url": "https:\/\/twitter.com\/SpaceX\/status\/1624150738447536128",
+        "tag": "#space",
+        "time": 1676122825
+    },
+    {
+        "id": 256,
+        "text": "HUBBLE on Twitter: \"\u2744\ufe0f Reminding us of the winter frost, this #HubbleTopImage shows part of the Large Magellanic Cloud, a nearby galaxy orbiting the Milky Way. Read more: https:\/\/t.co\/3p8bqgj1AV \ud83d\udcf7 @esa \/ @HUBBLE_space & @NASA Ack.: J. Barrington https:\/\/t.co\/TYXHK3AVYQ\" \/ Twitter",
+        "url": "https:\/\/twitter.com\/HUBBLE_space\/status\/1605941468060852224",
+        "tag": "#space",
+        "time": 1671901967
+    },
+    {
+        "id": 250,
+        "text": "HUBBLE on Twitter: \"\ud83c\udf84Just in time for the festive season, this Picture of the Week shows a small region of a nebula 7000 light-years from Earth. The image hosts a variety of interesting features, which you can read all about here https:\/\/t.co\/50U68j8M5X \ud83d\udcf7 @esa \/ @HUBBLE_space & @NASA R. Sahai https:\/\/t.co\/jKbH8ysTHu\" \/ Twitter",
+        "url": "https:\/\/twitter.com\/HUBBLE_space\/status\/1604748524066951168",
+        "tag": "#space #tmp #otherTag",
+        "time": 1671480576
+    },
+    {
+        "id": 245,
+        "text": "NASA Webb Telescope on Twitter: \"You can\u2019t escape its clutches. Just in time for #Halloween, the Pillars of Creation reach back out like a ghostly hand. The eerie landscape, captured this time by Webb\u2019s mid-infrared instrument (MIRI), spotlights ancient curtains of dust in new detail: https:\/\/t.co\/Y9QQBf9nYM https:\/\/t.co\/rumIH8J6rX\" \/ Twitter",
+        "url": "https:\/\/twitter.com\/NASAWebb\/status\/1585996531705978881",
+        "tag": "#space #tmp",
+        "time": 1668444918
+    },
+    {
+        "id": 239,
+        "text": "Cosmic Wonders - YouTube",
+        "url": "https:\/\/www.youtube.com\/watch?v=24cV8GYe-iw",
+        "tag": "#space #tmp",
+        "time": 1666791629
+    }
+]
