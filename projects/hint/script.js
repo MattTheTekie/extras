d090479a6dce5927f07c2138bc20b5d16d7bd40d
gabc123123 <gabc123123@gmail.com>
2023-03-05 04:04:09 +0200

    update

--- projects/hint/script.js
+++ projects/hint/script.js
@@ -1,11 +1,13 @@
-// v.1.1.3
+// v.1.1.4
 // not for large json files like in this example ! 
 
 function hint(printId, hintJsonVar, hintLimit, inputId){
 
-if(hintLimit == undefined||hintLimit == ''){ hintLimit = 35; }
+if(hintLimit == null||hintLimit == undefined||hintLimit == ''){ hintLimit = 35; }
+if(inputId == null||inputId == undefined||inputId == ''){ inputId = ''; }
 var com = '';
-var inputA = '';
+
+
 
 // input
 if(document.querySelectorAll('textarea').length >= 1){
@@ -16,14 +18,14 @@ com = 'textarea';
 
 
 
-if(document.querySelectorAll('input').length >= 1){
+if(document.querySelectorAll('input').length >= 2){
 inputA = document.querySelectorAll('input')[0];
 inputA.addEventListener('input', updateValueInput);
 com = 'input';
 }
 
 
-if(document.getElementById(inputId) != null&&document.getElementById(inputId) == ''){
+if(document.getElementById(inputId) != null&&document.getElementById(inputId) != ''&&inputId != ''){
 inputA = document.getElementById(inputId);
 inputA.addEventListener('input', updateValueInput);
 com = 'inputId';
@@ -34,17 +36,13 @@ com = 'inputId';
 var print = '';
 
 
-function updateValueInput(e) {
-var q = encodeURIComponent(e.target.value);
-//var q = e.target.value;
-
-
-var q2 = (' '+q).split(' ');
-
+function updateValueInput(e){
+//var qInput = encodeURIComponent(e.target.value);
+var qInput = (e.target.value);
+//var qInput = e.target.value;
+var q2 = (' '+qInput).split(' ');
 var lastInputSymbol = q2[q2.length - 1];
-
 if(lastInputSymbol.length >= 1){
-
 var count = 0;
 for (let index = 0; index < hintJsonVar.length; index++) {  
 const item = hintJsonVar[index];  
@@ -58,7 +56,7 @@ if(item2 != undefined&&item2.slice(0, lastInputSymbol.length) == lastInputSymbol
 var item3 = item2.replace(/'/g, "\\'"); // https://stackoverflow.com/questions/15087497/escaping-single-quotes-in-javascript-string-for-javascript-evaluation
 
 print += `
-<a class="tag light border2" onclick="insertText('${q}', '${item3}', '${com}', '${inputId}');" style="cursor:pointer;">${item2}</a>
+<a class="tag light border2" onclick="insertText('${qInput}', '${item3}', '${com}', '${inputId}');" style="cursor:pointer;">${item2}</a>
 `;
 count++;
 if(count >= hintLimit){
@@ -75,12 +73,12 @@ ${print}
 <!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist -->
 `;
 
-
 // print hint
 document.getElementById(printId).innerHTML = print;
 print = '';
 
 }
+}
 
 
 
@@ -91,13 +89,15 @@ print = '';
 
 }
 
-}
+
+
+
+
 
 function insertText(q, text, com, inputId){
 q = q.split(' ');
 q.pop();
 q = q.join(" ");
-
 
 
 if(com == 'textarea'){
