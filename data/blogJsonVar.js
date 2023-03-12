bd6d84b1b923bf0786c1034c9799eacb4a69d6b4
gabc123123 <gabc123123@gmail.com>
2023-03-12 17:36:06 +0200

    update

--- data/blogJsonVar.js
+++ data/blogJsonVar.js
@@ -1,6 +1,13 @@
 var blogJsonVar = 
 [
     {
+        "id": "232",
+        "text": "Hubble on Twitter: \"A million-second exposure. 10,000 galaxies. One mind-blowing image. The Hubble Ultra Deep Field image unveiled a powerful, deep view of our universe #OTD in 2004. This image only covers the amount of sky you can see through an eight-foot-long soda straw: https:\/\/t.co\/zaMJELUaJz https:\/\/t.co\/xbvxPFQPKn\" \/ Twitter",
+        "url": "https:\/\/twitter.com\/NASAHubble\/status\/1633853586349207552",
+        "tag": "#space",
+        "time": 1678635277
+    },
+    {
         "id": "231",
         "text": "NASA Webb Telescope on Twitter: \"Triples is best. This Webb image features a special galaxy that appears 3 times. Why? There's a galaxy cluster here whose mass and gravity are so great that time and space around it gets warped. This magnifies, multiplies, and distorts galaxies behind it: https:\/\/t.co\/ftRTPC4bDr https:\/\/t.co\/fzTjUGc5UO\" \/ Twitter",
         "url": "https:\/\/twitter.com\/NASAWebb\/status\/1630591812204109832",
