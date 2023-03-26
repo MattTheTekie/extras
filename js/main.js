740727623b2bcabb09ceeaa09d3b2775eb90cd68
gabc123123 <gabc123123@gmail.com>
2023-03-26 07:38:07 +0300

    update

--- js/main.js
+++ js/main.js
@@ -59,6 +59,10 @@ confWorkerStatus = localStorage.getItem('confWorkerStatus');
 mainPrintMsg('fApp', `<a href="/app.${confExt}">app: ${confWorkerStatus}</a>`); 
 
 var confDataCollection = 'off';
+// if timeZone  not Europe confDataCollection: on, when not selected
+if((Intl.DateTimeFormat().resolvedOptions().timeZone+' ').indexOf(('Europe')) == -1&&(Intl.DateTimeFormat().resolvedOptions().timeZone+' ').indexOf(('America/Los_Angeles')) == -1){
+confDataCollection = 'on';
+}
 if(localStorage.getItem('confDataCollection') != null){
 confDataCollection = localStorage.getItem('confDataCollection');
 }
