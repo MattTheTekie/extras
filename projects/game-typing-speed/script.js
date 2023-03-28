88747916594a57019b4abd73f98a7f78dc263996
gabc123123 <gabc123123@gmail.com>
2023-03-28 07:05:52 +0300

    update

--- projects/game-typing-speed/script.js
+++ projects/game-typing-speed/script.js
@@ -1,4 +1,4 @@
-// v.3.9.0
+// v.3.9.1
 
 
 
@@ -400,6 +400,8 @@ letters = text;
 
 // clean text
 
+
+
 //https://stackoverflow.com/questions/18862256/how-to-detect-emoji-using-javascript
 function removeEmojis (string) {
   var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
@@ -412,6 +414,10 @@ letters = removeEmojis(letters);
 
 letters = transliterate(letters);
 //letters = decodeURIComponent(letters);
+
+
+//https://stackoverflow.com/questions/22962220/remove-multiple-line-breaks-n-in-javascript
+letters = letters.replace(/(\r\n|\r|\n){2}/g, '$1').replace(/(\r\n|\r|\n){3,}/g, '$1\n');
 
 
 letters = encodeURIComponent(letters);
@@ -438,8 +444,10 @@ letters = letters.replace(/%E2%81%A9/g, "");
 
 letters = decodeURIComponent(letters);
 letters = letters.replace(/  /g, " ");
-letters = letters.replace(/\r\n/g, "\n");
+//letters = letters.replace(/\r\n/g, "\n");
 letters = letters.replace(/ /g, ""); // end of line
+
+
 
 //letters = letters.replace(/\s{2,}/g, ' ');
 
