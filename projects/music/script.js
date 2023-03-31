208e48025db8b625f279f9bb10d8b4f374219fa3
gabc123123 <gabc123123@gmail.com>
2023-03-31 23:39:45 +0300

    update

--- projects/music/script.js
+++ projects/music/script.js
@@ -4,6 +4,7 @@
 
 var json = musicFPJsonVar;
 
+//https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
