b067063a859257c5374e6c9eb8aec35ab6f567ae
gabc123123 <gabc123123@gmail.com>
2023-04-16 18:02:17 +0300

    update

--- js/main.js
+++ js/main.js
@@ -37,9 +37,7 @@ mainPrintMsg('footer', `
 <span id="fApp"><a href="/app.${confExt}">app: status</a></span>
 <span id="fPrivacy"><a href="/privacy.${confExt}">cookie: status</a></span>
 
-
-<span><a href="https://twitter.com/gabcpr">🐦 twitter</a></span>
-<span><a href="/rss.xml">⋮ rss</a></span>
+<span><a href="/rss.xml">rss</a></span>
 
 
 <span id="fAbout"><a href="/about.${confExt}">about</a></span>
