c02c4e295be43f091807d1eabb56b94cf2085ca2
gabc123123 <gabc123123@gmail.com>
2023-04-24 13:22:45 +0300

    update

--- projects/time-stopwatch/script.js
+++ projects/time-stopwatch/script.js
@@ -1,4 +1,4 @@
-// v.1.3.3
+// v.1.4.0
 // at the end was inspired by Google Clock
 
 var secArr = [];
@@ -10,6 +10,8 @@ var lStopArr = [];
 
 function fuStopwatchCom(com){
 
+
+
 if(com == 'pause'){ comArr[0] = 'pause'; }
 if(com == 'stop'&&comArr[0] != 'pause'){ comArr[0] = 'stop'; }
 if(com == 'continue'){ comArr[0] = 'continue'; }
@@ -17,6 +19,10 @@ if(com == 'refresh'){
 secArr[0] = 0;  comArr[0] = 'refresh'; lStopArr = [];
 document.getElementById('result3').innerHTML = '';
 document.getElementById('chart').innerHTML = '';
+}
+
+if(com == 'stop/refresh'){
+comArr[0] = 'stop/refresh';
 }
 
 // https://stackoverflow.com/questions/457826/pass-parameters-in-setinterval-function
@@ -27,6 +33,7 @@ document.getElementById('panel').innerHTML = `
 <a class="padding margin" onclick="fuStopwatchCom('refresh')" href="#">refresh</a>
 <a class="padding margin" onclick="fuStopwatchCom('pause')" href="#">pause</a>
 <a class="padding margin" onclick="fuStopwatchCom('stop')" href="#">stop</a>
+<a class="padding margin" onclick="fuStopwatchCom('stop/refresh')" href="#">stop/refresh</a>
 `;
 }
 
@@ -70,6 +77,8 @@ if(comArr[0] == 'pause'){ sec = secArr[0]; }
 
 
 
+
+
 let hours = normalize(Math.floor(sec / 3600));
 let minutes = normalize(Math.floor(sec % 3600 / 60));
 let seconds = normalize(Math.floor(sec % 3600 % 60));
@@ -81,13 +90,16 @@ let hours2 = normalize(time2.getHours());
 let minutes2 = normalize(time2.getMinutes());
 let seconds2 = normalize(time2.getSeconds());
 
+var chart = '';
 
-if(comArr[0] == 'stop'){
+if(comArr[0] == 'stop'||comArr[0] == 'stop/refresh'){
 let lResult = '';
 lStopArr.push(hours+':'+minutes+':'+seconds);
+if(comArr[0] == 'stop/refresh'){ secArr[0] = 0; chart = '2'; }
+
 document.getElementById('result3').innerHTML = '';
 //https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-lStopArr.sort().reverse().forEach((element) => {
+lStopArr.reverse().forEach((element) => {
 lResult += 
 `
 <span>${element}</span>
@@ -104,16 +116,24 @@ comArr[0] = 'stoped';
 //chart
 var chartTitle = '';
 var chartData = '';
+var element33 = '';
 //https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 lStopArr.reverse().forEach((element, index) => {
 //https://stackoverflow.com/questions/7709803/javascript-get-minutes-between-two-dates
-var element33 = Math.abs(new Date('2011/10/09 '+lStopArr[index]) - new Date('2011/10/09 '+lStopArr[index + 1]));
+if(chart != 2){
+element33 = Math.abs(new Date('2011/10/09 '+lStopArr[index]) - new Date('2011/10/09 '+lStopArr[index + 1]));
+}else{
+element33 = Math.abs(new Date('2011/10/09 '+lStopArr[index]) - new Date('2011/10/09 00:00:00'));
+}
 
 //https://www.w3schools.com/jsref/jsref_isNaN.asp
 if(!isNaN(element33)){
-element33 = Math.floor((element33/1000));
+//element33 = Math.floor((element33 / 1000)); // sec
+element33 = Math.floor((element33 / 1000 / 60)); // min
+//element33 = Math.floor((element33 / 1000 / 60 / 60)); // hour
 chartTitle += new String((element33+',').replaceAll(':', ''));
 chartData += new String(element33)+',';
+
 }
 
 
@@ -130,12 +150,10 @@ chartData = new String(chartData).slice(0, -1);
 //console.log(chartTitle);
 
 document.getElementById('chart').innerHTML = `
-<img src="https://quickchart.io/chart?c=%7Btype:%27bar%27,data:%7Blabels:[${chartTitle}],datasets:[%7Blabel:%27sec%27,data:[${chartData}]%7D]%7D%7D" width="500" alt="chart">
+<img src="https://quickchart.io/chart?c=%7Btype:%27bar%27,data:%7Blabels:[${chartTitle}],datasets:[%7Blabel:%27min%27,data:[${chartData}]%7D]%7D%7D" width="500" alt="chart">
 `;
 
 }
-
-
 
 
 
