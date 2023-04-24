cf9e74d4d0d684607c37ff8606df14618ca97a09
gabc123123 <gabc123123@gmail.com>
2023-04-24 17:31:18 +0300

    update

--- projects/time-stopwatch/script.js
+++ projects/time-stopwatch/script.js
@@ -149,9 +149,9 @@ chartTitle = new String(chartTitle).slice(0, -1);
 chartData = new String(chartData).slice(0, -1);
 //console.log(chartTitle);
 
-document.getElementById('chart').innerHTML = `
+/*document.getElementById('chart').innerHTML = `
 <img src="https://quickchart.io/chart?c=%7Btype:%27bar%27,data:%7Blabels:[${chartTitle}],datasets:[%7Blabel:%27min%27,data:[${chartData}]%7D]%7D%7D" width="430" alt="chart">
-`;
+`;*/
 
 }
 
