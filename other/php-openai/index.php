7337031bf9bc0a1c71c715c92517841e6633f03c
gabc123123 <gabc123123@gmail.com>
2023-05-08 08:48:55 +0300

    update

--- other/php-openai/index.php
+++ other/php-openai/index.php
@@ -1,88 +1,37 @@
 <?php
-
-$q = 'Hello';
-if(isset($_GET['q'])){ $q = $_GET['q']; }
-if(isset($_POST['q'])){ $q = $_POST['q']; }
-
-
-
-/*
-curl https://api.openai.com/v1/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-  "model": "text-davinci-003",
-  "prompt": "",
-  "temperature": 0.7,
-  "max_tokens": 256,
-  "top_p": 1,
-  "frequency_penalty": 0,
-  "presence_penalty": 0
-}'
-
-*/
+if(php_uname('s') == 'Windows NT'){
+$dirname = explode("\\", getcwd());
+$dirname = $dirname[count(explode('\\', getcwd()))-1];
+$d = $_SERVER["DOCUMENT_ROOT"].'/';
+}else{
+$dirname = explode("/", getcwd());
+$dirname = $dirname[count(explode("/", getcwd()))-1];
+$d = $_SERVER["DOCUMENT_ROOT"].'/';
+}
 
 
+include_once $d.'top.php';
+include_once 'get-api.php';
 
-include_once $d.'config2.php';
-//include_once $d.'top.php';
+echo <<<e
+div id="form" class="wrapperL">
 
-// openai php query using Curl
+<form  style="margin-top: 3px;" method="GET" action="index.php" autocomplete="off">
 
-// https://stackoverflow.com/questions/8115683/php-curl-custom-headers
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"https://api.openai.com/v1/completions");
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Content-Type: application/json",
-    "Authorization: Bearer $OPENAI_API_KEY"
-]);
-//https://stackoverflow.com/questions/11079135/how-to-post-json-data-with-php-curl
-$arr = json_encode(array(
-"model" => "text-davinci-003",
-"prompt" => "$q",
-"temperature"=> 0.7,
-"max_tokens" => 256,
-"top_p" => 1,
-"frequency_penalty" => 0,
-"presence_penalty" => 0,
-));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $arr );
-//https://stackoverflow.com/questions/16338174/saving-a-curl-response-into-a-php-variable
-curl_setopt($ch, CURLOPT_HEADER, 0);  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+<input id="input2" class="padding" type="search" style="text-align: center;" name="q" autofocus="" placeholder="" list="hint2" autocomplete="off" value="$q">
 
-$server_output = curl_exec($ch);
-curl_close ($ch);
+<input class="xSamll" style="padding: 0; min-height: 1px; height: 24px;" type="submit">
 
-
-// full response
-//print_r($server_output);
-
-
-// print
-$text = explode('[{"text":"', $server_output);
-$text = rawurldecode(strtok($text[1], '"'));
-//https://stackoverflow.com/questions/3264270/replace-n-with-actual-new-line-character-code
-echo str_replace(array('\r\n', '\n\r', '\n', '\r'), '
-', $text);
+</form>
 
 
 
 
 
+</div>
+e;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+include_once $d.'bottom.php';
 
 ?>
