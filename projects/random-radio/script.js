6bb243681b8613ac6419ff7e9cea9b293a7f0dd3
gabc123123 <gabc123123@gmail.com>
2023-05-09 11:36:44 +0300

    update

--- projects/random-radio/script.js
+++ projects/random-radio/script.js
@@ -1,4 +1,4 @@
-// v.1.2.3
+// v.1.2.4
 
 
 function randomRadio(printId, jsonVar){
@@ -538,7 +538,7 @@ return tagList;
 
 
 document.getElementById(printId).innerHTML += `
-<div class="center tCenter">
+<div class="center tCenter margin padding">
 <div class="wrapper3">
 
 <span class="op small padding margin">list of tags:</span><br>
