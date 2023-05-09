3c39e90b6632b26c8d03ed8934fed9732df6c9fb
gabc123123 <gabc123123@gmail.com>
2023-05-09 07:00:11 +0300

    update

---
+++ bottom.php
@@ -0,0 +1,18 @@
+
+
+
+
+</div>
+
+
+<footer id="footer"></footer>
+
+
+<script src="/js/main.js"></script>
+<script src="/data/adsJsonVar.js"></script>
+<script src="/js/ad.js"></script>
+<script> fuAds('', 'ads2', ''); </script>
+
+
+</body>
+</html>
---
+++ top.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Page</title>
+
+<link rel="alternate" type="application/rss+xml" title="<br />
+<b>Warning</b>:  Undefined variable $confSiteName in <b>/mnt/d/Sync/htdocs/github/page/top.php</b> on line <b>42</b><br />
+ Blog RSS" href="/rss.xml">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--<meta name="title" content="">-->
+<meta name="description" content="">
+<!--<meta name="keywords" content="">-->
+
+<!--<meta name="color-scheme" content="light dark">--><!--twitter embed light corner-->
+<!--<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">-->
+<meta name="generator" content="">
+
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="-1">
+
+<!--<meta name="referrer" content="no-referrer" />--><!-- YouTube embed video not playing -->
+<meta name="theme-color" media="(prefers-color-scheme: light)" content="#F2F2F2"><!-- comment for validator -->
+<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#3E3E3E">
+
+<link rel="icon" sizes="512x512" href="/img/logo.png" type="image/png">
+<!--<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />-->
+
+
+
+<!-- start main css -->
+<link rel="preload" href="/js/main.js" as="script">
+
+<link rel="stylesheet" type="text/css" href="/css/style-main.css" id="theme">
+<link rel="stylesheet" type="text/css" href="/css/style-main.css">
+<noscript><link rel="stylesheet" type="text/css" href="/css/light.css"></noscript>
+<!-- end -->
+
+
+
+
+
+<script src="/js/main.js"></script>
+
+<link rel="canonical" href="<br />
+<b>Warning</b>:  Undefined variable $confSiteUrl2 in <b>/mnt/d/Sync/htdocs/github/page/top.php</b> on line <b>94</b><br />
+">
+</head>
+<body>
+ 
+<!-- content -->
+<div class="content">
+
+<!--<div style="text-align: center;"><b>Page</b></div>-->
